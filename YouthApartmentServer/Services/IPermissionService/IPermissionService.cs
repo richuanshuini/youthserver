1e6619cbff8d27715b3d7b3e4fb7ebcb8717566b
qqqq <qwp520520@163.com>
2025-10-24 11:03:00 +0800

    更新了几个接口

--- YouthApartmentServer/Services/IPermissionService/IPermissionService.cs
+++ YouthApartmentServer/Services/IPermissionService/IPermissionService.cs
@@ -1,6 +1,11 @@
-﻿namespace YouthApartmentServer.Services.IPermissionService;
+﻿using YouthApartmentServer.Model.UserPermissionModel;
+
+namespace YouthApartmentServer.Services.IPermissionService;
 
 public interface IPermissionService
 {
-    
+    Task<List<Permission>> GetAllPermissionsAsync();
+    Task<Permission?> GetPermissionByIdAsync(int id);
+    Task<Permission?> CreatePermissionAsync(Permission permission);
+    Task<int> DeletePermissionAsync(int id);
 }
--- YouthApartmentServer/Services/IPermissionService/PermissionService.cs
+++ YouthApartmentServer/Services/IPermissionService/PermissionService.cs
@@ -1,6 +1,35 @@
-﻿namespace YouthApartmentServer.Services.IPermissionService;
+﻿using YouthApartmentServer.Model.UserPermissionModel;
+using YouthApartmentServer.Repositories.IPermission;
+
+namespace YouthApartmentServer.Services.IPermissionService;
 
 public class PermissionService:IPermissionService
 {
+    private readonly IPermissionRepository _permissionRepository;
+
+    public PermissionService(IPermissionRepository permissionRepository)
+    {
+        _permissionRepository = permissionRepository;
+    }
+    public async Task<List<Permission>> GetAllPermissionsAsync()
+    {
+        return await _permissionRepository.GetAllAsync();
+    }
+
+    public async Task<Permission?> GetPermissionByIdAsync(int id)
+    {
+        return await _permissionRepository.GetByIdAsync(id);
+    }
     
+    public async Task<Permission?> CreatePermissionAsync(Permission permission)
+    {
+        return await _permissionRepository.InsertAsync(permission);
+    }
+
+    public async Task<int> DeletePermissionAsync(int id)
+    {
+        return await _permissionRepository.DeleteByIdAsync(id);
+    }
 }
+
+    
