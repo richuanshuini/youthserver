9cc9dd4a07a276f7c65e602e6775a2d807588085
qqqq <qwp520520@163.com>
2025-11-13 13:03:42 +0800

    更新

---
+++ YouthApartmentServer/Model/PropertyManagementModel/Appointment.cs
@@ -0,0 +1,27 @@
+﻿namespace YouthApartmentServer.Model.PropertyManagementModel;
+
+using FreeSql.DataAnnotations;
+
+public enum Appointmentstatus
+{
+    Pending=0,    //待确认
+    Confirmed=1,    //已确认
+    Cancelled=2,    //已取消
+    Viewed=3,    //已看房
+    Missed=4    //爽约
+}
+
+public class Appointment
+{
+    public int AppointmentId{get;set;} //预约ID
+    public int UserId{get;set;} //预约用户
+    public int ProPertyId{get;set;} //房源ID
+    public int AssignedStaffId{get;set;} //员工ID，作为User的外键
+    public string? Remarks {get;set;} //备注
+    public DateTime AppointMentTime{get;set;}  //预约开始时间
+    public DateTime AppointMentEndTime { get; set; } //预约结束时间
+    public DateTime? AssignmentTime{get;set;} //分配该预约时间，若未分配员工，则可为Null
+    public DateTime CreateAt{get;set;} //创建时间，由ORM决定，初次插入时决定
+    public Appointmentstatus Status { get; set; } = Appointmentstatus.Pending; //预约状态
+    public bool IsDelete{get;set;}=false; //软删除
+}
--- YouthApartmentServer/Model/PropertyManagementModel/Property.cs
+++ YouthApartmentServer/Model/PropertyManagementModel/Property.cs
@@ -36,24 +36,24 @@ public class Property
     public int PropertyId { get; set; } //房源ID
     public int RegionId { get; set; } //区域ID
     public int ApprovedById { get; set; } //审核员ID，作为User表的外键
+    public int Area { get; set; } //面积
+    public int Bedrooms { get; set; } //卧室数量
+    public int Bathrooms { get; set; } //浴室数量
     public string? PropertyName { get; set; } // 房源名称
     public string? Address { get; set; } //地址
+    public string? Description { get; set; } //描述
     public decimal RentPrice { get; set; } //租赁价格
     public decimal RentDeposit { get; set; } //租赁押金
     public decimal PropertyFee { get; set; } //物业费
     public decimal Latitude { get; set; } //经度
     public decimal Lngitude { get; set; } //纬度
+    public PropertyStatus Status { get; set; } = PropertyStatus.PendingReview; //状态
     public LeaseType LeaseType { get; set; } = LeaseType.WholeRent; //租赁类型
     public LeaseTerm LeaseTerm { get; set; } = LeaseTerm.Month; //租赁期限
-    public int Area { get; set; } //面积
-    public int Bedrooms { get; set; } //卧室数量
-    public int Bathrooms { get; set; } //浴室数量
-    public string? Description { get; set; } //描述
-    public PropertyStatus Status { get; set; } = PropertyStatus.PendingReview; //状态
-    public bool IsDeleted { get; set; } = false; //软删除逻辑
     public DateTime AvailableDate { get; set; } //预约后可入住时间
-    public DateTime CraeteAt { get; set; } = DateTime.Now; //创建时间
+    public DateTime CraeteAt { get; set; } //创建时间，由ORM决定，初次插入时决定
     public DateTime UpdateAt { get; set; } = DateTime.Now; //更新时间
     public DateTime ApprovedBy { get; set; } //审核时间
     public DateTime DeleteAt { get; set; } //被删除时间
+    public bool IsDeleted { get; set; } = false; //软删除逻辑
 }
